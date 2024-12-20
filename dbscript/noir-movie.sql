5c8336de64aecfdbe0da85179f90db8daa77c4dc
miharimeedum <miharimeedum@gmail.com>
2024-12-20 05:50:39 +0530

    DB Updates

--- dbscript/noir-movie.sql
+++ dbscript/noir-movie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2024 at 09:49 PM
+-- Generation Time: Dec 20, 2024 at 01:18 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
